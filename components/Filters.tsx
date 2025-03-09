5efee772a9ddf7ca5c5a963c07870d07353cdb6c
abhijith2213 <abhijithas2213@gmail.com>
2025-03-09 15:13:37 +0400

    explore page and home done

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -10,16 +10,16 @@ const Filters = () => {
     const handleCategoryPress =(category:string)=>{
         if(selectedCategory === category){
             setSelectedCategory('All')
-            router.setParams({ filter: 'all'})
+            router.setParams({ filter: 'All'})
             return
         }
         setSelectedCategory(category)
-        router.setParams({ filter: 'category'})
+        router.setParams({ filter: category})
     }
     return (
         <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mt-3 mb-2">
             {categories.map((item,index)=>(
-                <TouchableOpacity onPress={()=>handleCategoryPress(item.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory === item.category ? 'bg-primary-300' : 'bg-primary-100 border border-primary-200'}`}>
+                <TouchableOpacity key={index} onPress={()=>handleCategoryPress(item.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory === item.category ? 'bg-primary-300' : 'bg-primary-100 border border-primary-200'}`}>
                     <Text className={`text-sm ${selectedCategory === item.category ? 'text-white font-rubik-bold mt-0.5': 'text-black-300 font-rubik'}`}>{item.title}</Text>
                 </TouchableOpacity>
             ))}
